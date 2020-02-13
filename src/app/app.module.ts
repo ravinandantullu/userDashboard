1c92776d3c006d2d8644c17b82c1b802fae80932
praveen <praveemkumar@network.lilly.com>
2020-02-13 17:23:25 -0500

    error-fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { InitialPageComponent } from './src/app/initial-page/initial-page.component';
-import { UserDetailsComponent } from './src/app/user-details/user-details.component';
-import { AddUserComponent } from './src/app/add-user/add-user.component';
+import { InitialPageComponent } from './initial-page/initial-page.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
   declarations: [
