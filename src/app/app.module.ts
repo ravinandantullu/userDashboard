f3905c4d863da27cbd2bdbce81c63c5f611484e3
praveen <praveemkumar@network.lilly.com>
2020-02-13 17:09:15 -0500

    please fix error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InitialPageComponent } from './src/app/initial-page/initial-page.component';
+import { UserDetailsComponent } from './src/app/user-details/user-details.component';
+import { AddUserComponent } from './src/app/add-user/add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitialPageComponent,
+    UserDetailsComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule
