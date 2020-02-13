90304964923a48f52cd07bc2cb4e305346aefccf
ravinandantullu <ravinandantulluri@gmail.com>
2020-02-13 11:57:45 -0800

    first commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { UserDashboardPage } from './app.po';
+
+describe('user-dashboard App', () => {
+  let page: UserDashboardPage;
+
+  beforeEach(() => {
+    page = new UserDashboardPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
