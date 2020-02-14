7771e7993aff663f63baa1c543a8da90b85e1402
ravinandantullu <ravinandantulluri@gmail.com>
2020-02-13 20:25:55 -0800

    adduser html added.

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-add-user',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddUserComponent implements OnInit {
 
+  addUserForm: FormGroup;
   constructor() { }
 
   ngOnInit() {
+    this.addUserForm = new FormGroup({
+      'firstname': new FormControl(null),
+      'lastname': new FormControl(null),
+      'email': new FormControl(null),
+      'phonenumber': new FormControl(null),
+      'address': new FormControl(null),
+      'city': new FormControl(null),
+      'state': new FormControl(null),
+      'zip': new FormControl(null),
+    });
+  }
+
+  addUser() {
+    console.log(this.addUserForm);
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { InitialPageComponent } from './initial-page/initial-page.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
+
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -17,7 +20,9 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
